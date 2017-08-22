9c5de9791873d4147e3363109e5b684bb474401c
Israel Vergara <israel.vergara102824@gmail.com>
2017-08-21 22:36:37 -0500

    Intercambio inicial de proyecto

---
+++ Tarjeta.java
@@ -0,0 +1,30 @@
+public class Tarjeta
+{
+    private float saldo;
+    
+    public Tarjeta()
+    {
+        saldo=0;
+    }
+    
+    public void depositar(float dinero)
+    {
+        saldo = saldo + dinero;
+    }
+    
+    public float retirar(float dinero)
+    {
+        if(saldo >= dinero)
+        {
+            saldo = saldo - dinero;
+            return dinero;
+        }
+        else
+            return 0;
+    }
+    
+    public float consulta()
+    {
+        return saldo;
+    }
+}
