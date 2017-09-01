db0094b75915def2e676596989dee89a558a4c05
Israel Vergara <israel.vergara102824@gmail.com>
2017-09-01 00:04:23 -0500

    se realiza documentacion de la clase como de sus metodos, se modifica el metodo retirar para que el saldo no se convierta en numeros negativos

--- Tarjeta.java
+++ Tarjeta.java
@@ -1,12 +1,24 @@
+/**
+ * clase que simula los movimientos minimos de una tarjeta bancaria.
+ * como son: depositar, retirar y consultar.
+ * @author Israel Vergara Navor.
+ */
 public class Tarjeta
 {
     private float saldo;
     
+    /**
+     * Constructor que inicializa la tarjeta en ceros.
+     */
     public Tarjeta()
     {
-        saldo=0;
+        saldo = 0;
     }
     
+    /**
+     * metodo que permite realizar un deposito
+     * @param dinero variable que almacena el dinero que indica el usuario
+     */
     public void depositar(float dinero)
     {
         if(dinero > 0){
@@ -14,17 +26,31 @@ public class Tarjeta
         }
     }
     
+    /**
+     * metodo que permite retirar dinero de la tarjeta.
+     * @param dinero variable que almacena la cantidad a retirar
+     * @return saldo variable que se regresa cuando el retiro es realizado 
+     * exitosamente con su previo descuento y tembien lo regresa si el 
+     * usuario quizo retirar mas de lo que tenia en la tarjeta para que pueda 
+     * ver cuanto es lo que tiene que retirar y este introduzaca una cantidad
+     * menor a la indicada.
+     */
     public float retirar(float dinero)
     {
-        if(saldo >= dinero)
-        {
-            saldo = saldo - dinero;
-            return dinero;
+        if(dinero > 0){
+            if(saldo >= dinero)
+            {
+                saldo = saldo - dinero;
+                return saldo;
+            }
         }
-        else
-            return 0;
+        return saldo;
     }
     
+    /**
+     * metodo que permite la consuta del saldo de la tarjeta
+     * @return saldo variable que contien el saldo de la tarjeta.
+     */
     public float consulta()
     {
         return saldo;
